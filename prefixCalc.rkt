94cc83d060e1801786b1cd38dd7822d0c446d5c2
Bhargav Nelapolu <73515711+s616627@users.noreply.github.com>
2025-03-28 22:55:50 -0500

    Add files via upload

--- prefixCalc.rkt
+++ prefixCalc.rkt
@@ -34,47 +34,76 @@
    "pow"    (lambda (a b) (expt a b))
    "mod"    (lambda (a b) (remainder a b))))
 
+;;evaluator
+(define (eval-expr tokens history))
+  (cond
+    [(null? tokens) (error "unexcpected end of input")]
+
+    ;;for the operator
+    [(member (firts-tokens) (hash-keys operator-table))
+     (define op (first tokens))
+     (define proc (hash-ref operator-table op))
+     cond(
+          ;;unary operator "-"
+          [(equal? op "-")
+           (define-values (a rest1) (eval-expr (rest tokens) history))
+           (values (proc a) rest1)]
+       ;; binary operators
+          [else
+           (define-values (a rest1) (eval-expr (rest tokens) history))
+           (define-values (b rest2) (eval-expr rest1 history))
+           (values (proc a b) rest2)])]
+    
+    ;; history reference
+    [(regexp-match #px"^\\$(\\d+)$" (first tokens))
+     => (lambda (m)
+          (define idx (string->number (second m)))
+          (if (and (positive? idx) (<= idx (length history)))
+              (values (list-ref (reverse history) (- idx 1)) (rest tokens))
+              (error "history reference out of range")))]
+
+        ;; numeric literal
+    [(string->number (first tokens))
+     => (lambda (n) (values n (rest tokens)))]
+
+    ;; anything else returns an error
+    [else (error "invalid token")]))
+
 ;;REPL (main calculator loop)
 (define (calculator)
   ;;loop of getting inputs
-  (define (loop)
+  (define (loop history)
     
-    (display ">") (flush-output)
+    (when interactive? (display ">") (flush-output))
     
     ;;get input
     (define input (read-line))
     
     ;;split input string
-    (define tokens (string-split input))
+    ;;(define tokens (string-split input))
 
     ;;if 'quit' is inputed, quit program
     (cond
-      [(or (null? tokens) (string=? (first tokens) "quit"))
-       (displayln "Goodbye")]
+      [(eof-object? input) (void)] ;; end-of-file ends batch mode
+      [(string=? input "quit")
+       (when interactive? (displayln "Goodbye")) (void)]
 
-      ;;otherwise
       [else
-       ;; get operation (first character)
-       (define op (first tokens))
-       ;;get numbers from expression (parse rest of input)
-       (define args (map string->number (rest tokens)))
+       (define tokens (filter (lambda (s) (not (string-blank? s))) (string-split input)))
        (cond
-         ;;if an operator cant be found using the hashtable, print out an error message
-         [(not (hash-has-key? operator-table op))
-          (displayln "Unknown operator.") (loop)]
+         [(null? tokens) (error-msg "Empty expression") (loop history)]
          [else
-          ;;handle errors
-          (define proc (hash-ref operator-table op))
-          (with-handlers ([exn:fail? (lambda (e)
-                                       (displayln "Error during calculation.")
-                                       (loop))]) ;; redo loop
-            ;;calculate and display the answer
-            (define result (apply proc args))
-            (displayln result)
-            ;;redo loop
-            (loop))])]))
-  ;;redo loop
-  (loop))
+          (with-handlers ([exn:fail? (lambda (e) (error-msg (exn-message e)) (loop history))])
+            (define-values (result rest) (eval-expr tokens history))
+            (if (null? rest)
+                (begin
+                  (define new-history (cons result history))
+                  (display-result (length new-history) result)
+                  (loop new-history))
+                (begin (error-msg "Leftover input after expression") (loop history))))])]))
 
-;;does the program
+  ;; start
+  (loop '()))
+
+;; --- Run ---
 (calculator)
